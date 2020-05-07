c370b093e8eec48ed26cfbacbcd54c29383ebe0c
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-05-06 22:15:57 -0300

    Create Pipedrive Handler

---
+++ api/pipedrive/pipedrive.handle.js
@@ -0,0 +1,98 @@
+
+const pipedriveService = require('./pipedrive.service')
+//const restifyErrors = require('restify-errors')
+const blingModel = require('../bling/bling.model')
+const axios = require('axios')
+const environment = require('../../common/environment')
+
+exports.getOpportunitiesWon = async (req, res, next) => {
+
+    try {
+
+        const result = await pipedriveService.urlPipedrive.get()
+
+        const Opportunities = await result.data.data.map((res) => {
+            let oportunity = {}
+            oportunity.customer = res.person_id.name,
+                oportunity.title = res.title,
+                oportunity.value = res.value,
+                oportunity.add_time = res.add_time,
+                oportunity.update_time = res.update_time
+            return oportunity
+        })
+
+
+        const postOpportunities = () => {
+            for (let i = 0; i < Opportunities.length; i++) {
+
+                const xmlOrder = `<?xml version="1.0" encoding="UTF-8"?>
+                <pedido>
+                <cliente>
+                    <nome>${Opportunities[i].customer}</nome>
+                </cliente>
+                <transporte>
+                    <volumes>
+                        <volume>
+                            <servico>SEDEX - CONTRATO</servico>
+                            <codigoRastreamento></codigoRastreamento>
+                        </volume>
+                    </volumes>
+                </transporte>
+                <itens>
+                    <item>
+                        <codigo>001</codigo>
+                        <descricao>Caneta 001</descricao>
+                        <un>Pç</un>
+                        <qtde>10</qtde>
+                        <vlr_unit>1.68</vlr_unit>
+                    </item>
+                    <item>
+                        <codigo>002</codigo>
+                        <descricao>Caderno 002</descricao>
+                        <un>Un</un>
+                        <qtde>3</qtde>
+                        <vlr_unit>3.75</vlr_unit>
+                    </item>
+                    <item>
+                        <codigo>003</codigo>
+                        <descricao>Teclado 003</descricao>
+                        <un>Cx</un>
+                        <qtde>7</qtde>
+                        <vlr_unit>18.65</vlr_unit>
+                    </item>
+                </itens>
+                <parcelas>
+                    <parcela>
+                        <data>01/09/2009</data>
+                        <vlr>${Opportunities[i].value}</vlr>
+                        <obs>Teste obs 1</obs>
+                    </parcela>
+                </parcelas>
+                <vlr_frete>15</vlr_frete>
+                <vlr_desconto>10</vlr_desconto>
+                <obs>Testando o campo observações do pedido</obs>
+                <obs_internas>Testando o campo observações internas do pedido</obs_internas>
+            </pedido>`
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                };
+                axios.post(`${environment.environment.urlBling}=${encodeURI(xmlOrder)}`, config)
+                    .then((res) => {
+                        console.log(res.data)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                //const element = Opportunities[i];
+                blingModel.OrderBling.create(Opportunities[i])
+            }
+        }
+
+        postOpportunities()
+        return res.json(Opportunities)
+    } catch (error) {
+        console.log(error)
+    }
+}
--- common/environment.js
+++ common/environment.js
@@ -4,5 +4,5 @@ exports.environment = {
     server: {port: process.env.SERVER_PORT || 4003},
     db: {url: process.env.DB_URL || 'mongodb://localhost/linkapi'},
     urlPipedrive: 'https://empresateste11.pipedrive.com/v1/deals?status=won&api_token=2957f556dc3b1b67ddfc4a9a89549c7b8d597de4',
-    urlBling: 'https://bling.com.br/Api/v2/pedido/'
+    urlBling: 'https://bling.com.br/Api/v2/pedido/?apikey=db81d67d8ab1d3dbf0b205dda53326017f66f33d50245d1d7f4d0a838e58b5b6a2101ae4&xml'
 }
